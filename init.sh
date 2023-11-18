076dea2ea6edc9d5485b9d3836ef1ddfe0316ea9
velho <jewel960@gmail.com>
2023-11-18 22:08:57 +0200

    yocto: add apt install to get deps for yocto

--- init.sh
+++ init.sh
@@ -3,6 +3,14 @@
 # initialises the yocto project for the zuboard 1cg
 # velho@2023
 
+# get dependencies for ubuntu
+lsb_release=/etc/lsb-release
+if [[ -d $lsb_release ]]; then
+    if grep -q "Ubuntu" $lsb_release; then
+        sudo apt install gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit mesa-common-dev zstd liblz4-tool file locales libacl1
+        sudo locale-gen en_US.UTF-8
+    fi
+fi
 # initializes submodules
 git submodule update --init --recursive
 
